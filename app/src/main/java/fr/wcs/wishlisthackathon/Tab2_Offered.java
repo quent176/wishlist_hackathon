0900f222199c17bef3738d6f62725f94eb70f5c9
quent176 <quentin7611@yahoo.fr>
2017-10-30 14:15:21 +0100

    Add Picasso Lib to read pics

--- app/src/main/java/fr/wcs/wishlisthackathon/Tab2_Offered.java
+++ app/src/main/java/fr/wcs/wishlisthackathon/Tab2_Offered.java
@@ -5,6 +5,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
 import static fr.wcs.wishlisthackathon.R.layout.tab2_offered;
 
 
@@ -17,7 +21,12 @@ public class Tab2_Offered extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         View rootView = inflater.inflate(tab2_offered, container, false);
+        ImageView testPicasso = (ImageView) rootView.findViewById(R.id.imageViewTest1);
+
+        Picasso.with(getContext()).load("http://i.imgur.com/DvpvklR.png").into(testPicasso);
+
         return rootView;
     }
 }
