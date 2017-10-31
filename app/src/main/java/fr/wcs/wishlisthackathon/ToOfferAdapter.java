97a143dc0e57c031292563ad77a11e52692fe72c
quent176 <quentin7611@yahoo.fr>
2017-10-31 12:17:56 +0100

    show name of user gift

--- app/src/main/java/fr/wcs/wishlisthackathon/ToOfferAdapter.java
+++ app/src/main/java/fr/wcs/wishlisthackathon/ToOfferAdapter.java
@@ -45,7 +45,7 @@ public class ToOfferAdapter extends BaseAdapter {
     public View getView(final int i, View convertView, final ViewGroup viewGroup) {
         if (convertView == null){
             convertView = LayoutInflater.from(viewGroup.getContext())
-                    .inflate(R.layout.layout_item, viewGroup, false);
+                    .inflate(R.layout.layout_item_tooffer, viewGroup, false);
         }
 
         convertView.setOnClickListener(new View.OnClickListener() {
@@ -65,6 +65,8 @@ public class ToOfferAdapter extends BaseAdapter {
         ImageView imgItem= convertView.findViewById(R.id.imageItem);
         textDescription.setText(currentItem.getObject_description());
         Picasso.with(context).load(currentItem.getObject_image()).resize(450,450).into(imgItem);
+        TextView textNameToOffer = convertView.findViewById(R.id.TextViewNameOfferTo);
+        textNameToOffer.setText("Pour : \n" +  currentItem.getObject_user_name());
 
         return convertView;
     }
